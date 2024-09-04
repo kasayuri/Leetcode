19e1bb6a7431182a78eadde1418ac77ee00b641e
Karina <60903427+kasayuri@users.noreply.github.com>
2024-09-04 08:34:50 -0300

    Update 1790-check-if-one-string-swap-can-make-strings-equal.cs

--- week_2/1790-check-if-one-string-swap-can-make-strings-equal.cs
+++ week_2/1790-check-if-one-string-swap-can-make-strings-equal.cs
@@ -1,1 +1,34 @@
 // https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal/description/
+
+public class Solution {
+    public bool AreAlmostEqual(string s1, string s2) {
+        char diffArray1 = '0';
+        char diffArray2 = '0';
+        int countDiff = 0;
+
+        //percorre caractere a caractere e compara se iguais. 
+        //Se n for, armazena em uma var diff
+        for(int i = 0; i < s1.Length; i++){
+            if (s1[i] != s2[i]){
+                countDiff++;
+                if (diffArray1 == '0'){
+                    diffArray1 = s1[i];
+                    diffArray2 = s2[i];
+                } //chars sao diferentes e tem algo no diff
+                else if (diffArray1 != s2[i] || diffArray2 != s1[i]){
+                    return false;
+                }
+            }
+        }
+
+        //se as duas forem iguais
+        if (diffArray1 == '0' && diffArray2 == '0') 
+            return true;
+
+        // verifica se houve exatamente 2 chars diferentes
+        if (countDiff != 2)
+            return false;
+
+        return true;
+    }
+}
