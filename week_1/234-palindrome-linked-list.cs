0fb3d86e354dc9b9bd716c478e0534d754334c65
Karina <60903427+kasayuri@users.noreply.github.com>
2024-09-07 08:32:45 -0300

    Update 234-palindrome-linked-list.cs
    closes #4

--- week_1/234-palindrome-linked-list.cs
+++ week_1/234-palindrome-linked-list.cs
@@ -1,1 +1,54 @@
-https://leetcode.com/problems/palindrome-linked-list/description/
+//https://leetcode.com/problems/palindrome-linked-list/description/
+
+//https://leetcode.com/problems/palindrome-linked-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+ 
+ // .  .,   .   ,       ,
+ // 1   2   3   4   5   6
+    
+ // .   .,  .   .,      ,       ,
+ // 1   2   3   4   5   6   7
+public class Solution {
+    public bool IsPalindrome(ListNode head) {
+        if (head == null)
+            return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode mid = head;
+
+        while(fast != null && fast.next != null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        mid = slow; //se par, considera o metade -1
+        Stack<int> pilha = new Stack<int>();
+
+        while(mid != null){
+            pilha.Push(mid.val);
+            mid = mid.next;
+        }
+
+        while (pilha.Any()){
+            if (head.val != pilha.Pop()){
+                return false;
+            }
+            head = head.next;
+        }
+
+        return true;
+    }
+}
