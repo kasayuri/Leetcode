59acb7da6cd919f25e7b8d121dc186f5bf4e7713
Karina <60903427+kasayuri@users.noreply.github.com>
2024-09-08 14:32:25 -0300

    Update 709. To Lower Case.cs

--- week_1/709. To Lower Case.cs
+++ week_1/709. To Lower Case.cs
@@ -1,5 +1,11 @@
 public class Solution {
     public string ToLowerCase(string s) {
-        return s.ToLower();
+        char[] result = s.ToCharArray();
+        for (int i = 0; i < s.Length; i++){
+            if (result[i] >= 'A' && result[i] <= 'Z') {
+                result[i] = (char)(result[i] + 32); // Convert uppercase to lowercase
+            } 
+        }
+        return new string(result);
     }
 }
