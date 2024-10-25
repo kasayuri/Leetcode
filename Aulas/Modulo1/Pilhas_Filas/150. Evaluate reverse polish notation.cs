c2cf84541c6acb5989c99ed44f545762a229a77e
Karina <60903427+kasayuri@users.noreply.github.com>
2024-10-25 17:19:11 -0300

    Update 150. Evaluate reverse polish notation.cs

--- Aulas/Modulo1/Pilhas_Filas/150. Evaluate reverse polish notation.cs
+++ Aulas/Modulo1/Pilhas_Filas/150. Evaluate reverse polish notation.cs
@@ -6,3 +6,41 @@ Quando encontrar um operador, retira os dois últimos operandos da pilha e reali
 Como a entrada é garantida ser uma expressão válida, ao final das operações a pilha conterá um único valor, que é a avaliação da expressão de entrada.
 */
 
+public class Solution {
+    public int EvalRPN(string[] tokens) {
+        
+        Stack<int> pilha = new Stack<int>();
+
+        foreach (string token in tokens) 
+        {
+            if (!"+-*/".Contains(token)) 
+            {
+                pilha.Push(Int32.Parse(token));
+                continue;
+            }
+
+            int var1 = pilha.Pop();
+            int var2 = pilha.Pop();
+            char op = Char.Parse(token);
+
+            pilha.Push(CalculaOperacao(var1, var2, op));
+        }
+
+        return pilha.Pop();
+    }
+
+    private int CalculaOperacao(int valor1, int valor2, char operador){
+        if (operador == '+'){
+            return valor2 + valor1;
+        } else if (operador == '-'){
+            return valor2 - valor1;
+        } else if (operador == '*'){
+            return valor2 * valor1;
+        } else if (operador == '/'){
+            return valor2/valor1;
+        } else{
+            return 0;
+        }
+
+    }
+}
