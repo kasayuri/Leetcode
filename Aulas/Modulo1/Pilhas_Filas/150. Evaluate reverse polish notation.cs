3f6647d9d64f872a2b05e559ba9cb6b2c266df2f
Karina <60903427+kasayuri@users.noreply.github.com>
2024-10-25 17:20:44 -0300

    Update 150. Evaluate reverse polish notation.cs

--- Aulas/Modulo1/Pilhas_Filas/150. Evaluate reverse polish notation.cs
+++ Aulas/Modulo1/Pilhas_Filas/150. Evaluate reverse polish notation.cs
@@ -1,6 +1,22 @@
 //https://leetcode.com/problems/evaluate-reverse-polish-notation/
 
 /*
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+
+    The valid operators are '+', '-', '*', and '/'.
+    Each operand may be an integer or another expression.
+    The division between two integers always truncates toward zero.
+    There will not be any division by zero.
+    The input represents a valid arithmetic expression in a reverse polish notation.
+    The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+*/
+
+/*
 Estratégia: percorrer o array de tokens empilhando os operandos.
 Quando encontrar um operador, retira os dois últimos operandos da pilha e realiza a operação, empilhando o resultado.
 Como a entrada é garantida ser uma expressão válida, ao final das operações a pilha conterá um único valor, que é a avaliação da expressão de entrada.
