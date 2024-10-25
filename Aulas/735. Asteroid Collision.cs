635817111b77865cabe7268795ddfdff3c7aacf7
Karina <60903427+kasayuri@users.noreply.github.com>
2024-10-25 13:25:53 -0300

    Update 735. Asteroid Collision.cs

--- Aulas/735. Asteroid Collision.cs
+++ Aulas/735. Asteroid Collision.cs
@@ -1,4 +1,11 @@
 // https://leetcode.com/problems/longest-common-subsequence/submissions/1433499687
+/*
+We are given an array asteroids of integers representing asteroids in a row.
+
+For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+
+Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+*/
 
 public class Solution {
     public int[] AsteroidCollision(int[] asteroids) {
