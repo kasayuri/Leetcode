34eb83570281e5bf6e749e707d709cd3c47c0be9
Karina <say.karina@hotmail.com>
2025-01-29 23:33:12 -0300

    add 347 (wip)

---
+++ Aulas/Modulo1/Pilhas_Filas/347. Top K Frequent Elements.py
@@ -0,0 +1,44 @@
+# https://leetcode.com/problems/top-k-frequent-elements/description/
+
+''''
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
+ 
+
+Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+'''
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        mapa = Counter(nums)
+        sorted_dict = OrderedDict(mapa)
+        
+        lista = [] #[item for item in sorted_dict.values()]
+
+        while len(lista) < k:
+            lista.append(sorted_dict.pop())
+
+        print(mapa)
+        print(sorted_dict)
+        print(lista)
+        b = heapq.heapify(sorted_dict)
+
+        return [item for item, _ in nlargest(k, count.items(), key=lambda x: x[1])]
